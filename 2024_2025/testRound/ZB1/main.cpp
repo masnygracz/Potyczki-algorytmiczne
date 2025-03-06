f9525f677f9ce12d51e29abc84606e0121eafe77
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-03-06 19:27:36 +0100

    Added cache to save recurring types of action on sets

--- 2024_2025/testRound/ZB1/main.cpp
+++ 2024_2025/testRound/ZB1/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n, m, q;
     cin >> n >> m >> q;
 
     int static_table[n + m][n] = {};
     vector<vector<int>> dynamic_table(n + m);
+    unordered_map<string, int> cache;
 
     // Adding numbers to static and dynamic table
     for (int i = 1; i <= n; i++) {
@@ -27,41 +32,61 @@ int main() {
             cin >> x >> y;
             x--; y--;
 
-            for (int num : dynamic_table[x]) {
-                if (!static_table[extra_set][num - 1]) {
-                    static_table[extra_set][num - 1] = 1;
-                    dynamic_table[extra_set].push_back(num);
+            string key = "U_" + to_string(x) + "_" + to_string(y);
+            if (cache.count(key)) {
+                extra_set = cache[key];
+            } else {
+                for (int num : dynamic_table[x]) {
+                    if (!static_table[extra_set][num - 1]) {
+                        static_table[extra_set][num - 1] = 1;
+                        dynamic_table[extra_set].push_back(num);
+                    }
                 }
-            }
-            for (int num : dynamic_table[y]) {
-                if (!static_table[extra_set][num - 1]) {
-                    static_table[extra_set][num - 1] = 1;
-                    dynamic_table[extra_set].push_back(num);
+                for (int num : dynamic_table[y]) {
+                    if (!static_table[extra_set][num - 1]) {
+                        static_table[extra_set][num - 1] = 1;
+                        dynamic_table[extra_set].push_back(num);
+                    }
                 }
+                cache[key] = extra_set;
             }
+
         }
         else if (type == 2) {  // Common part
             int x, y;
             cin >> x >> y;
             x--; y--;
 
-            for (int num : dynamic_table[x]) {
-                if (static_table[y][num - 1]) {
-                    static_table[extra_set][num - 1] = 1;
-                    dynamic_table[extra_set].push_back(num);
+            string key = "I_" + to_string(x) + "_" + to_string(y);
+            if (cache.count(key)) {
+                extra_set = cache[key];
+            } else {
+                for (int num : dynamic_table[x]) {
+                    if (static_table[y][num - 1]) {
+                        static_table[extra_set][num - 1] = 1;
+                        dynamic_table[extra_set].push_back(num);
+                    }
                 }
+                cache[key] = extra_set;
             }
+
         }
         else if (type == 3) {  // Complement of a set
             int x;
             cin >> x;
             x--;
 
-            for (int i = 1; i <= n; i++) {
-                if (!static_table[x][i - 1]) {
-                    static_table[extra_set][i - 1] = 1;
-                    dynamic_table[extra_set].push_back(i);
+            string key = "N_" + to_string(x);
+            if (cache.count(key)) {
+                extra_set = cache[key];
+            } else {
+                for (int i = 1; i <= n; i++) {
+                    if (!static_table[x][i - 1]) {
+                        static_table[extra_set][i - 1] = 1;
+                        dynamic_table[extra_set].push_back(i);
+                    }
                 }
+                cache[key] = extra_set;
             }
         }
         extra_set++;
