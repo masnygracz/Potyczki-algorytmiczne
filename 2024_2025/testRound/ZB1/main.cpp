d8a049e013f6678f80fa10963bdf67506be7b100
Jakub Woszczek <icefreestyle2000@gmail.com>
2025-03-06 19:49:24 +0100

    Added bit-map for faster computing, xd

--- 2024_2025/testRound/ZB1/main.cpp
+++ 2024_2025/testRound/ZB1/main.cpp
@@ -1,104 +1,90 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 #include <unordered_map>
+
 using namespace std;
+
+struct pair_hash {
+    size_t operator()(const pair<int, int>& p) const {
+        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+    }
+};
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    const int MAX_N = 50000;
+    bitset<MAX_N> sets[450000];
+    unordered_map<pair<int, int>, int, pair_hash> cache;
+    unordered_map<int, int> negation_cache;
+
     int n, m, q;
     cin >> n >> m >> q;
 
-    int static_table[n + m][n] = {};
-    vector<vector<int>> dynamic_table(n + m);
-    unordered_map<string, int> cache;
-
-    // Adding numbers to static and dynamic table
     for (int i = 1; i <= n; i++) {
         for (int j = i; j <= n; j += i) {
-            dynamic_table[i - 1].push_back(j);
-            static_table[i - 1][j - 1] = 1;
+            sets[i - 1][j - 1] = 1;
         }
     }
 
     int extra_set = n;
+
     while (m--) {
         int type;
         cin >> type;
 
-        if (type == 1) {  // Sum of sets
+        if (type == 1) {  // Suma zbiorów
             int x, y;
             cin >> x >> y;
             x--; y--;
 
-            string key = "U_" + to_string(x) + "_" + to_string(y);
-            if (cache.count(key)) {
-                extra_set = cache[key];
+            if (cache.count({x, y})) {
+                extra_set = cache[{x, y}];
             } else {
-                for (int num : dynamic_table[x]) {
-                    if (!static_table[extra_set][num - 1]) {
-                        static_table[extra_set][num - 1] = 1;
-                        dynamic_table[extra_set].push_back(num);
-                    }
-                }
-                for (int num : dynamic_table[y]) {
-                    if (!static_table[extra_set][num - 1]) {
-                        static_table[extra_set][num - 1] = 1;
-                        dynamic_table[extra_set].push_back(num);
-                    }
-                }
-                cache[key] = extra_set;
+                sets[extra_set] = sets[x] | sets[y];
+                cache[{x, y}] = extra_set;
             }
-
         }
-        else if (type == 2) {  // Common part
+        else if (type == 2) {  // Przecięcie zbiorów
             int x, y;
             cin >> x >> y;
             x--; y--;
 
-            string key = "I_" + to_string(x) + "_" + to_string(y);
-            if (cache.count(key)) {
-                extra_set = cache[key];
+            if (cache.count({x, y})) {
+                extra_set = cache[{x, y}];
             } else {
-                for (int num : dynamic_table[x]) {
-                    if (static_table[y][num - 1]) {
-                        static_table[extra_set][num - 1] = 1;
-                        dynamic_table[extra_set].push_back(num);
-                    }
-                }
-                cache[key] = extra_set;
+                sets[extra_set] = sets[x] & sets[y];
+                cache[{x, y}] = extra_set;
             }
-
         }
-        else if (type == 3) {  // Complement of a set
+        else if (type == 3) {  // Negacja zbioru
             int x;
             cin >> x;
             x--;
 
-            string key = "N_" + to_string(x);
-            if (cache.count(key)) {
-                extra_set = cache[key];
+            if (negation_cache.count(x)) {
+                extra_set = negation_cache[x];
             } else {
-                for (int i = 1; i <= n; i++) {
-                    if (!static_table[x][i - 1]) {
-                        static_table[extra_set][i - 1] = 1;
-                        dynamic_table[extra_set].push_back(i);
-                    }
-                }
-                cache[key] = extra_set;
+                sets[extra_set] = ~sets[x];
+                sets[extra_set] &= bitset<MAX_N>().set();
+                negation_cache[x] = extra_set;
             }
         }
+
         extra_set++;
     }
 
+    // Obsługa zapytań
     while (q--) {
         int x, v;
         cin >> x >> v;
         x--; v--;
 
-        if (static_table[x][v]) cout << "TAK\n";
-        else cout << "NIE\n";
+        cout << (sets[x][v] ? "TAK\n" : "NIE\n");
     }
+
     return 0;
 }
